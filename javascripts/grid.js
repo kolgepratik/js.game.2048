b905ab3bbcec3dff19b30b9d240d0b82a7eeef6d
kolgepratik <kolge.pratik@gmail.com>
2014-03-24 13:20:11 +0530

    Added hints and 2 new game modes.

--- javascripts/grid.js
+++ javascripts/grid.js
@@ -16,6 +16,10 @@ var _control_bindings = {
 	HELP: 72,
 	RESTART: 82,
 	UNDO: 90,
+    MODE_SELECT: 77,
+    HINT: 78,
+    SAVE_GAME: 86,
+    LOAD_GAME: 76,    
     TOGGLE_BOOST: 66
 };
 
@@ -24,12 +28,16 @@ var _settings = {
 	PADDING_SIZE: 20,
 	MULTIPLIER: 2,
 	MAX_PAIRS: 2,
-	MAX_UNDO_MOVES: 4
+	MAX_UNDO_MOVES: 4,
+    BEST_SCORE_COOKIE_NAME: 'bestScoreCookie',
+    SAVE_GAME_COOKIE_NAME: 'saveUserGameCookie',
+    LOAD_GAME_COOKIE_NAME: 'loadUserGameCookie'
 };
 
 var _symbols = {
 	EMPTY: 0,
-	ONE: 2
+	ONE: 2,
+    THREE_ONE: 1
 };
 
 var _boosters = {
@@ -51,6 +59,39 @@ var _multipliers = [
     2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768
 ];
 
+var _symbolMappings = {
+    a: {        
+        2: 'A',
+        4: 'B',
+        8: 'C',
+        16: 'D',
+        32: 'E',
+        64: 'F',
+        128: 'G',
+        256: 'H',
+        512: 'I',
+        1024: 'J',
+        2048: 'K',
+        4096: 'L',
+        8192: 'M',
+        16384: 'N',
+        32768: 'O'
+    }, 
+    t: {
+        1: '1',
+        2: '2',
+        3: '3',
+        6: '6',
+        12: '12',
+        24: '24',
+        48: '48',
+        96: '96',
+        192: '192',
+        384: '384',
+        768: '768'
+    }
+};
+
 var _states = {
 	NS: 0,
 	IP: 1,
@@ -59,10 +100,16 @@ var _states = {
 	FN: 4
 };
 
+var _sets = {
+    DF: 0,
+    AL: 1,
+    TR: 2
+};
+
 var _user = {
 	score: { c: { s: 0, m: 0, maxLevel: _symbols.ONE }, b: { s: 0, m: 0, maxLevel: _symbols.ONE } },
 	game: { boost: _boosters.NONE, state: _states.IP, recentMoves: [] },
-    settings: { boost: false }
+    settings: { boost: false, set: _sets.TR }
 };
 
 var _a_left = [ [], [], [], [] ]; 
@@ -159,6 +206,29 @@ function _welcome() {
     
 }
 
+function _chooseMode() {
+    var $chooseSetContent = $('<div style="text-align: left; padding: 10px;"></div>');
+	
+	var $select = $('<select id="chooseModeSelect"></select>');		
+	$select.append($('<option></option>', { value: 0, html: 'Default' }));
+    $select.append($('<option></option>', { value: 1, html: 'Alphabets' }));
+	$select.append($('<option></option>', { value: 2, html: 'Threes' }));	
+    
+    var $restartButton = $('<button></button>', { type: 'button', text: 'Start' });
+	$restartButton.on('click', function() {
+		_user.settings.set = Number($('#chooseModeSelect option:selected').val());
+        $('#message').empty('');
+		$('#messageContainer').hide('puff', 500);
+		_restart();
+	});
+	
+	_user.game.state = _states.PS; 
+	
+	$('#message').empty().append($chooseSetContent.append($('<b>Select a mode and then click on the Start button to begin.</b><br><br>')).append('Mode: ').append($select).append($restartButton).append('<br><br>Press Spacebar to return to your Game.'));
+	
+	$('#messageContainer').show('puff', 200);
+}
+
 function _gameOver() {
 	var $gameOverContent = $('<div></div>');
 	var $score = $('<span><b>Game over</b><br></span>');
@@ -192,12 +262,13 @@ function _resume() {
 function _pause() {
 	var $helpContent = $('<div style="text-align: left; padding: 10px;"></div>');
 	
-	var $help = $('<ul></ul>');	
-	$help.append($('<li></li>', { html: 'Spacebar: Pause or Resume' }));
-	$help.append($('<li></li>', { html: 'Z: Undo your last move (maximum 4 moves)' }));
+	var $help = $('<ul></ul>');		
+    $help.append($('<li></li>', { html: 'M: Select Game Mode' }));
+	$help.append($('<li></li>', { html: 'Z: Undo your last move (maximum 4 moves)' }));    
     $help.append($('<li></li>', { html: 'B: Toggle Boosts (default: off)' }));
 	$help.append($('<li></li>', { html: 'R: Restart' }));
 	$help.append($('<li></li>', { html: 'H: Help & Controls' }));
+    $help.append($('<li></li>', { html: 'Spacebar: Pause or Resume' }));
 	
 	_user.game.state = _states.PS; 
 	
@@ -314,6 +385,19 @@ function saveUserMove(arr, where) {
 	_user.game.recentMoves.push({ a: moveArray, score: score, w: where });
 }
 
+
+function _saveUserGame() {
+    $.cookie(_settings.SAVE_GAME_COOKIE_NAME, JSON.stringify(_user));
+    message('Your Game was saved.');
+}
+
+
+function _loadUserGame() {
+    _user = $.parseJSON($.cookie(_settings.SAVE_GAME_COOKIE_NAME));
+    message('Saved Game loaded successfully.');
+}
+
+
 function _help() {
 	var $helpContent = $('<div style="text-align: left; padding: 10px;"></div>');
 	
@@ -331,6 +415,45 @@ function _help() {
 	$('#messageContainer').show('puff', 200);
 }
 
+
+function _getHint() {
+    var possibleMoves = 0;
+    var simulation = _simulateMoves();
+    
+    var $simulationContent = $('<div style="text-align: left; padding: 10px;"></div>');
+	
+	var $results = $('<div></div>');		
+    if(simulation.l.movement) {
+        $results.append('You can move <b>Left</b>' + (simulation.l.merged > 0 ? (' - This will merge <b>' + simulation.l.merged + '</b> block(s). Your score will increase by <b>' + simulation.l.plusScore + ' point(s)</b>') : '') + '<br><br>');    
+        possibleMoves++;
+    }
+    
+    if(simulation.t.movement) {
+        $results.append('You can move <b>Top</b>' + (simulation.t.merged > 0 ? (' - This will merge <b>' + simulation.t.merged + '</b> block(s). Your score will increase by <b>' + simulation.t.plusScore + ' point(s)</b>') : '') + '<br><br>');    
+        possibleMoves++;
+    }
+    
+    if(simulation.d.movement) {
+        $results.append('You can move <b>Down</b>' + (simulation.d.merged > 0 ? (' - This will merge <b>' + simulation.d.merged + '</b> block(s). Your score will increase by <b>' + simulation.d.plusScore + ' point(s)</b>') : '') + '<br><br>');    
+        possibleMoves++;
+    }
+    
+    if(simulation.r.movement) {
+        $results.append('You can move <b>Right</b>' + (simulation.r.merged > 0 ? (' - This will merge <b>' + simulation.r.merged + '</b> block(s). Your score will increase by <b>' + simulation.r.plusScore + ' point(s)</b>') : '') + '<br><br>');    
+        possibleMoves++;
+    }
+    
+    if(possibleMoves === 0) {
+        $results.append($('No hints available')); 
+    }
+    	
+	_user.game.state = _states.PS; 
+	
+	$('#message').empty().append($simulationContent.append($('<b>Hints</b><br><br>')).append($results).append('<br><br>Press Spacebar to resume your Game.'));
+	
+	$('#messageContainer').show('puff', 200);
+}
+
 function bindControls() {
 	$(document).on('keydown', function(event) {
 		if(_user.game.state === _states.IP || _user.game.state === _states.PF) {
@@ -342,7 +465,9 @@ function bindControls() {
 				_move(_controls.MOVE_DOWN);
 			} else if(event.which ===_control_bindings.RIGHT) {
 				_move(_controls.MOVE_RIGHT);
-			} 
+			} else if(event.which === _control_bindings.HINT) {
+                _getHint();
+            }
 		}
         
         if(_user.game.state === _states.IP || _user.game.state === _states.PF || _user.game.state === _states.FN) {
@@ -358,8 +483,18 @@ function bindControls() {
 		} else if(event.which ===_control_bindings.RESTART) {
 			_restart();
 		} else if(event.which ===_control_bindings.TOGGLE_BOOST) {
-			_toggleBoost();
-        }
+            if(_user.settings.set === _sets.DF) {
+                _toggleBoost();
+            } else {
+                message('Boosts are available only in Default Mode.<br>Change Game Mode by pressing <b>M</b> any time.');
+            }
+        } else if(event.which ===_control_bindings.MODE_SELECT) {
+            _chooseMode();
+        } else if(event.which ===_control_bindings.SAVE_GAME) {
+            _saveUserGame();
+        } else if(event.which ===_control_bindings.LOAD_GAME) {
+            _loadUserGame();
+        } 
 	});
 }
 
@@ -369,15 +504,28 @@ function animateBlock(blockElement, blockProperties) {
 	});
 }
 
+function getNewSymbolValue(value) {
+    var newValue = _symbols.EMPTY;
+    if(_user.settings.set === _sets.DF) {
+        newValue = value;
+    } else if(_user.settings.set === _sets.AL) {
+        newValue = _symbolMappings.a[value];
+    } else if(_user.settings.set === _sets.TR) {
+        newValue = _symbolMappings.t[value];
+    }
+    
+    return newValue;
+}
+
 function _refresh(newRandomBlock) {
 	var $blocks = $('#grid .block');
 	for(var b=0; b<_grid.length; b++) {
 		var $b = $($blocks[b]);
-		$b.removeClass($b.attr('class')).addClass('block block_' + b + ' level_' + (_grid[b].s));
+		$b.removeClass($b.attr('class')).addClass('block block_' + b + ' level_' + _user.settings.set + '_' +  (_grid[b].s));
 		if(_grid[b].s === _symbols.EMPTY) {
 			$b.find('span').html('');
 		} else {
-			$b.find('span').html(_grid[b].s);
+			$b.find('span').html(getNewSymbolValue(_grid[b].s));
 		}
 		
 		if(_grid[b].c) {
@@ -405,6 +553,17 @@ function getFreeGrid() {
     return freeGrid;
 }
 
+function getDefaultSymbol() {
+    var symbol = null;
+    if (_user.settings.set === _sets.DF || _user.settings.set === _sets.AL) {
+       symbol = _symbols.ONE;
+    } else if (_user.settings.set === _sets.TR) {
+       symbol = _symbols.THREE_ONE; 
+    }
+    
+    return symbol;
+}
+
 function _randomBlock() {
     var freeGrid = getFreeGrid();
 	
@@ -419,7 +578,7 @@ function _randomBlock() {
 		};
 		
 		if(retries < 10) {
-			_grid[freeGrid[rndm]].s = _symbols.ONE;
+			_grid[freeGrid[rndm]].s = getDefaultSymbol();
 			
 			_refresh(freeGrid[rndm]);
 		} else {
@@ -618,45 +777,162 @@ function checkMovement(arr) {
     return stateChanged;
 }
 
+function checkMovementAfterSimulation(originalArray, newArray) {
+    var stateChanged = false;
+    for(var i=0; i<originalArray.length; i++) {
+        for(var j=0; j<originalArray.length; j++) {
+            if(originalArray[i][j] !== newArray[i][j]) {
+                stateChanged = true;
+                break;
+            }
+        }
+        if(stateChanged) {
+            break;
+        }
+    }
+    
+    return stateChanged;
+}
+
+function _simulateMoves() {
+    var originalLeftArray = [];
+    var originalTopArray = [];
+    var originalDownArray = [];
+    var originalRightArray = [];
+    
+    var stateChanged = null;
+    var afterMove = null;
+    
+    var simulationResults = {};
+        
+    // Simulate left 
+    stateChanged = false;
+    afterMove = null;
+    getLeftArray();
+    // Backup left array 
+	for(var i=0; i <_a_left.length; i++) {
+		var temp = new Array();
+		for(var j=0; j<_a_left.length; j++) {
+			temp.push(_a_left[i][j]);
+		}
+		originalLeftArray.push(temp);
+	}
+    afterMove = _processMove(_a_left, _controls.MOVE_LEFT);
+    stateChanged = checkMovementAfterSimulation(originalLeftArray, _a_left);
+    simulationResults.l = { movement: stateChanged, merged: afterMove.p, plusScore: afterMove.s };
+    
+    // Simulate Top 
+    stateChanged = false;
+    afterMove = null;
+    getTopArray();
+    // Backup Top array     
+	for(var i=0; i <_a_top.length; i++) {
+		var temp = new Array();
+		for(var j=0; j<_a_top.length; j++) {
+			temp.push(_a_top[i][j]);
+		}
+		originalTopArray.push(temp);
+	}
+    afterMove = _processMove(_a_top, _controls.MOVE_TOP);
+    stateChanged = checkMovementAfterSimulation(originalTopArray, _a_top);
+    simulationResults.t = { movement: stateChanged, merged: afterMove.p, plusScore: afterMove.s };
+    
+    // Simulate Down 
+    stateChanged = false;
+    afterMove = null;
+    getDownArray();
+    // Backup Down array     
+	for(var i=0; i <_a_down.length; i++) {
+		var temp = new Array();
+		for(var j=0; j<_a_down.length; j++) {
+			temp.push(_a_down[i][j]);
+		}
+		originalDownArray.push(temp);
+	}
+    afterMove = _processMove(_a_down, _controls.MOVE_DOWN);
+    stateChanged = checkMovementAfterSimulation(originalDownArray, _a_down);
+    simulationResults.d = { movement: stateChanged, merged: afterMove.p, plusScore: afterMove.s };
+    
+    // Simulate Right 
+    stateChanged = false;
+    afterMove = null;
+    getRightArray();
+    // Backup Right array     
+	for(var i=0; i <_a_right.length; i++) {
+		var temp = new Array();
+		for(var j=0; j<_a_right.length; j++) {
+			temp.push(_a_right[i][j]);
+		}
+		originalRightArray.push(temp);
+	}
+    afterMove = _processMove(_a_right, _controls.MOVE_RIGHT);
+    stateChanged = checkMovementAfterSimulation(originalRightArray, _a_right);
+    simulationResults.r = { movement: stateChanged, merged: afterMove.p, plusScore: afterMove.s };    
+    
+    return simulationResults;
+}
+
 function _move(where) {
     var stateChanged = false;
-    var plussed = 0;
+    var afterMove = null;
+    
 	if(where === _controls.MOVE_LEFT) {
+        saveUserMove(_a_left, where);
+        
 		getLeftArray();
 		
-		plussed = _processMove(_a_left, _controls.MOVE_LEFT);
+		afterMove = _processMove(_a_left, _controls.MOVE_LEFT);
 		
         stateChanged = checkMovement(_a_left);
         
 		setLeftArray();
 	} else if(where === _controls.MOVE_TOP) {
+        saveUserMove(_a_top, where);
+        
 		getTopArray();
 		
-		plussed = _processMove(_a_top, _controls.MOVE_TOP);
+		afterMove = _processMove(_a_top, _controls.MOVE_TOP);
         
         stateChanged = checkMovement(_a_top);
 		
 		setTopArray();
 	} else if(where === _controls.MOVE_DOWN) {
+        saveUserMove(_a_down, where);
+        
 		getDownArray();
 		
-		plussed = _processMove(_a_down, _controls.MOVE_DOWN);
+		afterMove = _processMove(_a_down, _controls.MOVE_DOWN);
         
         stateChanged = checkMovement(_a_down);
 		
 		setDownArray();
 	} else if(where === _controls.MOVE_RIGHT) {
+        saveUserMove(_a_right, where);
+        
 		getRightArray();
 		
-		plussed = _processMove(_a_right, _controls.MOVE_RIGHT);
+		afterMove = _processMove(_a_right, _controls.MOVE_RIGHT);
         
         stateChanged = checkMovement(_a_right);
 		
 		setRightArray();
 	}
 	
+    _user.score.c.s += afterMove.s;
+    
+    if (afterMove.s !== 0) {
+        $('.scorePlus').hide();
+        $('#scorePlus').html(afterMove.s).show().hide('explode', 500);
+    }
+        
+    if(_user.game.boost !== _boosters.NONE) {
+        consumedBoost();
+    }
+	
+	_user.score.c.m++;
+    
 	_refresh();
-	if(_user.game.state === _states.PF && plussed === 0) {
+	if(_user.game.state === _states.PF && afterMove.p === 0) {
 		_gameOver();
 	} else {
         if(getFreeGrid().length === 0) {
@@ -671,8 +947,7 @@ function _move(where) {
 
 function _processMove(arr, where) {
 	var plussed = 0; 
-	
-	saveUserMove(arr, where);
+    var scorePlussed = 0;
 	
 	for(var itr=0; itr<arr.length; itr++) {		
 		var derived = new Object();
@@ -690,7 +965,7 @@ function _processMove(arr, where) {
 								j = 0;
 							}
 							arr[itr][i] = arr[itr][j];
-							arr[itr][j] = _symbols.EMPTY;							
+							arr[itr][j] = _symbols.EMPTY;
 						} else {
 							while(j>=0) {
 								arr[itr][j+1] = arr[itr][j];
@@ -701,41 +976,64 @@ function _processMove(arr, where) {
 					} else {
 						while(j>0 && arr[itr][j] === _symbols.EMPTY) {
 							j--;
-						};
+						};						
 						
-						if((arr[itr][i] === arr[itr][j]) && !(derived['' + i] || derived['' + j])) {
-							if(_user.game.boost !== _boosters.NONE) {
-								arr[itr][i] *= _user.game.boost;								
-							} else {
-								arr[itr][i] *= _settings.MULTIPLIER;
-							}
-							arr[itr][j] = _symbols.EMPTY;
-							derived['' + i] = true;
-							plussed++;
-							_user.score.c.s += arr[itr][i];
-							if(_user.score.c.maxLevel < arr[itr][i]) {
-								_user.score.c.maxLevel = arr[itr][i];
-							}
-							animateMove(itr, j, i, where, arr[itr][i]);
-						} else {
-							arr[itr][i - 1] = arr[itr][j];
-							if((i - 1) !== j) {
-								arr[itr][j] = _symbols.EMPTY;
-							}
-						}
+                        if(_user.settings.set === _sets.DF || _user.settings.set === _sets.AL) {
+                            if((arr[itr][i] === arr[itr][j]) && !(derived['' + i] || derived['' + j])) {
+                                if(_user.game.boost !== _boosters.NONE) {
+                                    arr[itr][i] *= _user.game.boost;								
+                                } else {
+                                    arr[itr][i] *= _settings.MULTIPLIER;
+                                }
+                                arr[itr][j] = _symbols.EMPTY;
+                                derived['' + i] = true;
+                                scorePlussed += arr[itr][i];
+                                plussed++;							
+                                if(_user.score.c.maxLevel < arr[itr][i]) {
+                                    _user.score.c.maxLevel = arr[itr][i];
+                                }
+                                animateMove(itr, j, i, where, arr[itr][i]);
+                            } else {
+                                arr[itr][i - 1] = arr[itr][j];
+                                if((i - 1) !== j) {
+                                    arr[itr][j] = _symbols.EMPTY;
+                                }
+                            }
+                        } else if(_user.settings.set === _sets.TR) {                            
+                            if((arr[itr][i] !== 2) && (arr[itr][i] === arr[itr][j]) && !(derived['' + i] || derived['' + j])) {                                
+                                arr[itr][i] += arr[itr][i];
+                                arr[itr][j] = _symbols.EMPTY;
+                                derived['' + i] = true;
+                                scorePlussed += arr[itr][i];
+                                plussed++;							
+                                if(_user.score.c.maxLevel < arr[itr][i]) {
+                                    _user.score.c.maxLevel = arr[itr][i];
+                                }
+                                animateMove(itr, j, i, where, arr[itr][i]);
+                            } else if((arr[itr][j] === 1 && arr[itr][i] === 2) || (arr[itr][j] === 2 && arr[itr][i] === 1) && !(derived['' + i] || derived['' + j])) {
+                                arr[itr][i] += arr[itr][j];
+                                arr[itr][j] = _symbols.EMPTY;
+                                scorePlussed += arr[itr][i];
+                                derived['' + i] = true;
+                                plussed++;							
+                                if(_user.score.c.maxLevel < arr[itr][i]) {
+                                    _user.score.c.maxLevel = arr[itr][i];
+                                }
+                                animateMove(itr, j, i, where, arr[itr][i]);
+                            } else {
+                                arr[itr][i - 1] = arr[itr][j];
+                                if((i - 1) !== j) {
+                                    arr[itr][j] = _symbols.EMPTY;
+                                }
+                            }
+                        }
 					}					
 				}
 			}			
 		} 
-	}
-    
-    if(_user.game.boost !== _boosters.NONE) {
-        consumedBoost();
-    }
+	}    
 	
-	_user.score.c.m++;
-	
-	return plussed;
+	return { p: plussed, s: scorePlussed };
 }
 
 function animateMove(level, start, end, where, newValue) {
@@ -780,7 +1078,4 @@ function animateMove(level, start, end, where, newValue) {
 		
 		$sb.animate({ left: _grid[e].p.l }, ((end - start) * 200), function() { $sb.remove(); $eb.remove(); });
 	}
-	
-	$('.scorePlus').hide();
-	$('#plus_' + newValue).show().hide('explode', 500);
 }
